ba5c177f9e912c894f3ae333fbd6348fdb31cf2d
Qurveball <lancehartley09@gmail.com>
2023-09-30 18:06:53 +0100

    After finishing the first mod tutorial. I added the item ruby and a raw version. I went on to add my own custom item, adamantine.

--- src/main/java/net/qurveball/firstmod/FirstMod.java
+++ src/main/java/net/qurveball/firstmod/FirstMod.java
@@ -2,6 +2,8 @@ package net.qurveball.firstmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.qurveball.firstmod.item.ModItemGroups;
+import net.qurveball.firstmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +13,7 @@ public class FirstMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModItemGroups.registerItemGroups();
 	}
 }
---
+++ src/main/java/net/qurveball/firstmod/item/ModItemGroups.java
@@ -0,0 +1,34 @@
+package net.qurveball.firstmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.qurveball.firstmod.FirstMod;
+
+public class ModItemGroups {
+
+    public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(FirstMod.MOD_ID, "ruby"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
+                    .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.RUBY);
+                        entries.add(ModItems.RAW_RUBY);
+                        entries.add(ModItems.ADAMANTINE);
+                        entries.add(ModItems.RAW_ADAMANTINE);
+
+
+                    }).build());
+
+    public static void registerItemGroups() {
+        FirstMod.LOGGER.info("Registering Item Groups for " + FirstMod.MOD_ID);
+
+    }
+
+
+}
